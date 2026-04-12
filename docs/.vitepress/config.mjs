2b4b20b08921f36cccad384dc35053324e30c67f
redersha <sh_515183.com>
2026-04-12 08:58:18 +0800

    添加搜索功能和目录功能的翻译

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -5,9 +5,42 @@ export default defineConfig({
   title: "大熊猫服务器文档v2",
   description: "教程、指南与规则",
   themeConfig: {
-    search: {
-      provider: 'local'
+    lastUpdated: {
+      text: "最后更新于",
+      formatOptions: {
+        dateStyle: "full",
+        timeStyle: "full",
+      },
     },
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+    darkModeSwitchLabel: "主题",
+    lightModeSwitchTitle: "切换至浅色",
+    darkModeSwitchTitle: "切换至深色",
+
+    sidebarMenuLabel: "菜单",
+    returnToTopLabel: "返回顶部",
     nav: [
       { text: '首页', link: '/' },
       { text: '快速指南', link: '/quick_guides/'},
